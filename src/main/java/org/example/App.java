6e0f312465a128f4bd8be076b2932332b38b119c
KarmandAziz <Karmand94@gmail.com>
2021-11-01 14:10:54 +0100

    Initial commit / Exercise1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.exercises.Exer1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Exer1 exercise = new Exer1();
+
+       exercise.example1();
+
+
+
+    }
+}
---
+++ src/main/java/org/example/exercises/Exer1.java
@@ -0,0 +1,31 @@
+package org.example.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exer1 {
+
+    public void example1(){
+
+        ArrayList<String> week = new ArrayList<>();
+        week.add("Monday");
+        week.add("Tuesday");
+        week.add("Wednesday");
+        week.add("Thursday");
+        week.add("Friday");
+        week.add("Saturday");
+        week.add("Sunday");
+
+        System.out.println(week);
+
+
+    }
+
+
+
+
+
+
+
+
+}
